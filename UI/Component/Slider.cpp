2c2bc80c34ac030f5f4f57a8885d1d529fabed87
Jc Hu <hujosh0509@gmail.com>
2025-05-25 00:37:35 +0800

    hackathon task 1 & 2

--- UI/Component/Slider.cpp
+++ UI/Component/Slider.cpp
@@ -15,12 +15,21 @@ Slider::Slider(float x, float y, float w, float h)
 }
 void Slider::Draw() const {
     // TODO HACKATHON-5 (3/4): The slider's component should be drawn here.
+    Bar.Draw();
+    End1.Draw();
+    End2.Draw();
+    ImageButton::Draw();    // button on the bar
 }
 void Slider::SetOnValueChangedCallback(std::function<void(float value)> onValueChangedCallback) {
+    // behavior is determined by caller
     OnValueChangedCallback = onValueChangedCallback;
 }
 void Slider::SetValue(float value) {
     // TODO HACKATHON-5 (4/4): Set the value of the slider and call the callback.
+    this->value = std::min(std::max(value, Min), Max);
+    Position.x = Bar.Position.x + Bar.Size.x * this->value; // update slider position
+    // will call SettingsScene::BGM/SFXSlideOnValueChanged(float)
+    OnValueChangedCallback(this->value);
 }
 void Slider::OnMouseDown(int button, int mx, int my) {
     if ((button & 1) && mouseIn)
